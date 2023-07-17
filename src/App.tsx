3dd821e60e917963a8d5490115333352fad559f8
edpadua <ed.padua@gmail.com>
2023-07-17 19:14:47 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+        <div className="bg-slate-500">
+          <h2>Teste</h2>
+        </div>
+    </>
+  )
+}
+
+export default App
